c8daa0c370cfb38604a7d2ac6f2532ab872bbb09
Atri Thakar <atrithaker122@gmail.com>
2026-05-16 21:19:46 +0530

    Configured express static file serving for map assets

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use('/maps/gtav', express.static(path.join(__dirname, 'gta_map')));
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
 
