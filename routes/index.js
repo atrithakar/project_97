6b57738301b8b49c7479d8cc048e618402bfd0ef
Atri Thakar <atrithaker122@gmail.com>
2026-05-17 23:33:02 +0530

    Registered photoRoutes to the main route controller

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const authRoutes = require('./auth.routes')
 const uploadRoutes = require('./upload.routes')
+const photoRoutes = require('./photo.routes')
 
 router.use('/', authRoutes)
 router.use('/', uploadRoutes)
+router.use('/', photoRoutes)
 
 module.exports = router;
