9ef886ccdb2b98b14c6ccef0faa1882be22b0096
Atri Thakar <atrithaker122@gmail.com>
2026-05-17 23:31:49 +0530

    Create a DB function to fetch photo info from the DB

--- models/photo.model.js
+++ models/photo.model.js
@@ -6,7 +6,7 @@ async function insertPhotoInDB(photo_obj) {
         photo_obj['id'],
         photo_obj['owner_id'],
         photo_obj['photo_name'],
-        photo_obj['thumbnail_name'],    
+        photo_obj['thumbnail_name'],
         photo_obj['title'],
         photo_obj['coord_x'],
         photo_obj['coord_y'],
@@ -20,4 +20,27 @@ async function insertPhotoInDB(photo_obj) {
     return queryResult
 }
 
-module.exports = { insertPhotoInDB }
+async function getFeedDB(offset, limit, seed) {
+
+    let orderBy = 'ORDER BY uploaded_at DESC'; // Default to newest first
+
+    if (seed) {
+        // MySQL RAND(N) produces a repeatable sequence based on the seed
+        orderBy = `ORDER BY RAND(${seed})`;
+    }
+
+    const query = `
+            SELECT p.id, u.username, p.thumbnail_name, p.photo_name, p.title, p.coord_x, p.coord_y 
+            FROM photos p
+            INNER JOIN users u
+            ON p.owner_id = u.id
+            ${orderBy} 
+            LIMIT ? OFFSET ?
+        `;
+
+    // Execute the query (assuming mysql2 promise wrapper)
+    const [queryResult] = await pool.query(query, [limit, offset]);
+    return queryResult
+}
+
+module.exports = { insertPhotoInDB, getFeedDB }
