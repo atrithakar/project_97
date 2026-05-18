70c4e3c2146274a11649b0df826cdd9b49b76bd4
Atri Thakar <atrithaker122@gmail.com>
2026-05-18 17:51:25 +0530

    Register photo info routes

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const authRoutes = require('./auth.routes')
 const uploadRoutes = require('./upload.routes')
 const photoRoutes = require('./photo.routes')
+const viewRoutes = require('./view.routes')
 
 router.use('/', authRoutes)
 router.use('/', uploadRoutes)
 router.use('/', photoRoutes)
+router.use('/view', viewRoutes)
 
 module.exports = router;
