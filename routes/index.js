0b58f4be46661aebc1ff37ba0ddebbf2f8ff0604
Atri Thakar <atrithaker122@gmail.com>
2026-05-19 22:59:15 +0530

    Registered profile routes

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const authRoutes = require('./auth.routes')
 const uploadRoutes = require('./upload.routes')
 const photoRoutes = require('./photo.routes')
 const viewRoutes = require('./view.routes')
+const profileRoutes = require('./profile.routes')
 
 router.use('/', authRoutes)
 router.use('/', uploadRoutes)
 router.use('/', photoRoutes)
+router.use('/', profileRoutes)
 router.use('/view', viewRoutes)
 
 module.exports = router;
