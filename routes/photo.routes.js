2ebc219a712b51d2186ebd1b375d2b19fa4327c4
Atri Thakar <atrithaker122@gmail.com>
2026-05-19 20:41:14 +0530

    Added routes for photo download, toggling photo favourite and checking favourite status of photo

--- routes/photo.routes.js
+++ routes/photo.routes.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const authenticateToken = require('../middlewares/authenticateToken.middleware');
-const {getFeed} = require('../controllers/photo.controller');
+const {getFeed, downloadPhotoFile} = require('../controllers/photo.controller');
+const { toggleFavourite, checkFavouriteStatus } = require('../controllers/favourite.controller');
 
 
 const router = express.Router();
 
 router.get('/api/photos/feed', authenticateToken, getFeed)
+router.get('/api/photos/download/:id', downloadPhotoFile)
+// router.js
+
+// GET checks the current status when the page loads
+router.get('/api/photos/:id/favourite/status', authenticateToken, checkFavouriteStatus);
+
+// POST toggles the state (adds if missing, removes if present)
+router.post('/api/photos/:id/favourite', authenticateToken, toggleFavourite);
+
+
 
 module.exports = router;
