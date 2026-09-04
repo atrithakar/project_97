92bae8591ca1c35f54f77c652e087ecf17a5a09b
Atri Thakar <atrithaker122@gmail.com>
2026-09-04 19:39:17 +0530

    Solved a bug

--- utils/genThumb.util.js
+++ utils/genThumb.util.js
@@ -1,11 +1,16 @@
 const sharp = require("sharp");
 const path = require("path")
+const fs = require('fs').promises;
+
 
 /**
  * 
  * @param {Buffer} fileBuffer 
  */
 async function generateThumbnail(fileBuffer, fileName) {
+    const outDir = path.join(__dirname, '..', 'uploads', 'thumbnails');
+    await fs.mkdir(outDir, { recursive: true });
+
     await sharp(fileBuffer).webp({quality: 75}).toFile(path.join(__dirname, '..', 'uploads', 'thumbnails', `THUMB${fileName}.webp`))
 }
 
